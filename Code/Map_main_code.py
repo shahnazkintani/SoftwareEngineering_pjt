71b6103106fa6bf2735617663d3d8626a1f5fdbf
Weaver_zzh <zzh9414@gmail.com>
2019-03-06 21:48:05 -0600

    Update Code and data, New program structure diagram

--- Code/Map_main_code.py
+++ Code/Map_main_code.py
@@ -7,14 +7,18 @@ Now put go_track_trackspoints.csv file under same file and open the map_john.htm
 2/18/2019: 
 Add in control flow to control the map creation and Data description
 2/25/2019:
-Clean up the code and package the functions, Add in control flow to control the point plot and heatmap creation
+Clean up the code and package the functions, Add in control flow to control the point/line plot and heatmap creation
 '''
 # Libraries & global values:
 import folium
 from folium import plugins
 import pandas as pd
-import csv
 
+#Initialize colors
+color_set = ['red', 'blue', 'green', 'purple', 'orange', 'darkred',
+				'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
+				'darkpurple', 'pink', 'lightblue', 'lightgreen',
+				'gray', 'black', 'lightgray']
 color_code = 0
 
 def display_Data_Info(data_name):
@@ -38,28 +42,40 @@ def specify_map_attr():
 def creat_map(DEF_LON,DEF_LAT):
 	#Map starts here
 	print("Starting Basic Layer Creation...")
-	return folium.Map(location=[DEF_LON, DEF_LAT], tiles='Stamen Toner', zoom_start=10)
+	return folium.Map(location=[DEF_LAT, DEF_LON], tiles='Stamen Toner', zoom_start=10)
 
 def change_color(color_set):
 	#Color change function
 	global color_code
-	print(color_code)
+	#print(color_code)
 	if color_code + 1 >= len(color_set):
 		color_code = 0
 	color_code = color_code + 1
 
 #Plot point onto the map for selected group 
-def plotPoints(Temp_trackID, color_code, color_set, m):
+def plot_marker(temp_trackID, color_code, color_set, m):
 	### (MARKED FOR DEBUG IN FOR STARTING LOOP RANGE)
-	for i in range(1, len(Temp_trackID)):
-		folium.Marker([Temp_trackID.iloc[i]['latitude'], Temp_trackID.iloc[i]['longitude']], popup = Temp_trackID.iloc[i]['time'], icon = folium.Icon(color=color_set[color_code])).add_to(m)
+	for i in range(1, len(temp_trackID)):
+		folium.Marker([temp_trackID.iloc[i]['latitude'], temp_trackID.iloc[i]['longitude']], popup = temp_trackID.iloc[i]['time'], icon = folium.Icon(color=color_set[color_code])).add_to(m)
 
-def heatmap(Temp_trackID, m):
+def plot_line(temp_tuple, color_code, color_set, m):
+	folium.PolyLine(temp_tuple, color=color_set[color_code], weight=2.5, opacity=1).add_to(m)
+
+def heatmap(temp_trackID, m):
 	# convert to (n, 2) nd-array format for heatmap
-	stationArr = Temp_trackID[['latitude', 'longitude']].values
+	stationArr = temp_trackID[['latitude', 'longitude']].values
 	# plot heatmap
 	m.add_child(plugins.HeatMap(stationArr, radius=15))
-
+	
+def plot_all(dataframed_data, color_code, color_set, m):
+	for i in range(1,len(dataframed_data)):
+		#change color upon change track_id
+		if (dataframed_data.iloc[i-1]['track_id'] != dataframed_data.iloc[i]['track_id']):
+			change_color(color_set)
+			folium.Marker([dataframed_data.iloc[i]['latitude'], dataframed_data.iloc[i]['longitude']], popup = dataframed_data.iloc[i]['time'], icon = folium.Icon(color=color_set[color_code])).add_to(m) 
+		else:
+			folium.Marker([dataframed_data.iloc[i]['latitude'], dataframed_data.iloc[i]['longitude']], popup = dataframed_data.iloc[i]['time'], icon = folium.Icon(color=color_set[color_code])).add_to(m)
+	
 def save_map(save_name, m):
 	#save map to current directory
 	print("Map created, save to:", save_name)
@@ -68,24 +84,18 @@ def save_map(save_name, m):
 def main():
 	
 	#Value area:
-	#Initialize colors
-	color_set = ['red', 'blue', 'green', 'purple', 'orange', 'darkred',
-				 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
-				 'darkpurple', 'pink', 'lightblue', 'lightgreen',
-				 'gray', 'black', 'lightgray']
-
 	# Set Default map location
-	DEF_LON = -10.93934139
-	DEF_LAT = -37.06274211
+	DEF_LAT = -10.93934139
+	DEF_LON = -37.06274211
 
 	select_flag = 'y'
 
 	#Map name
-	save_name = 'map_john.html'
+	save_name = 'Map_Created.html'
 
 	# read data from csv file (Ask for file location)
 	print("Reading Data From File...")
-	data_name = pd.read_csv("40_dp_test.csv")
+	data_name = pd.read_csv("25_car.csv")
 	print("Framing Data...")
 	dataframed_data = pd.DataFrame(data_name)
 	
@@ -97,13 +107,33 @@ def main():
 	#Data manipulation area
 	ID_grouped = dataframed_data.groupby('track_id')
 	
+	All_flag = input("Plot All polints? (y/n): ")
+	if All_flag == 'y':
+		plot_all(dataframed_data, color_code, color_set, m)
+		select_flag = 'n'
+	
 	while select_flag != 'n':
 		select_group = eval(input("Please input the Track id number you wish to display: "))
-		if select_group <= 4:
-			Temp_trackID = ID_grouped.get_group(select_group)
-			#Write selected group to map
-			plotPoints(Temp_trackID, color_code, color_set, m)
-			heatmap(Temp_trackID, m)
+		if select_group <= 25:
+			temp_trackID = ID_grouped.get_group(select_group)
+			#Extract lat and lon
+			print("Extracting Lattitude and Longitude...")
+			temp_long = list(temp_trackID.longitude)
+			temp_lati = list(temp_trackID.latitude)
+			temp_tuple = list(zip(temp_lati, temp_long))
+			#For each track id, select below options
+			marker_flag = input("Plot marker? (y/n): ")
+			if marker_flag == 'y':
+				plot_marker(temp_trackID, color_code, color_set, m)
+			
+			line_flag = input("Plot line? (y/n): ")
+			if line_flag == 'y':
+				plot_line(temp_tuple, color_code, color_set, m)
+			
+			heat_flag = input("Plot heatmap? (y/n): ")
+			if heat_flag == 'y':
+				heatmap(temp_trackID, m)
+			
 			select_flag = input("Select another id? (y/n): ")
 			if select_flag == 'y':
 				change_color(color_set)
@@ -117,18 +147,10 @@ def main():
 	
 	save_map(save_name, m)
 
-
 if __name__== "__main__":
 	main()
 	
 	
-
-'''
-print("Extracting Longitude and Lattitude...")
-Temp_long = list(Temp_trackID.longitude)
-Temp_lati = list(Temp_trackID.latitude)
-'''
-
 '''	
 #Plot all the points 	
 for i in range(1,len(dataframed_data)):
@@ -140,7 +162,6 @@ for i in range(1,len(dataframed_data)):
 			folium.Marker([dataframed_data.iloc[i]['latitude'], dataframed_data.iloc[i]['longitude']], popup = dataframed_data.iloc[i]['time'], icon = folium.Icon(color=color_set[color_code])).add_to(m)
 	
 '''
-
 
 '''
 	['red', 'blue', 'green', 'purple', 'orange', 'darkred',
